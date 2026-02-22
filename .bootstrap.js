9b7470d38721ef9ddd8c77f418410a75711b3ddc
Vibesdk <vibesdk-bot@cloudflare.com>
2026-02-22 15:36:54 +0000

    chore: Update bootstrap script with latest commands

--- .bootstrap.js
+++ .bootstrap.js
@@ -77,7 +77,12 @@ function updateWranglerJsonc() {
 }
 
 function runSetupCommands() {
-    const commands = [];
+    const commands = [
+    "bun add xterm-addon-web-links@^0.9.0",
+    "bun add xterm-addon-search@^0.13.0",
+    "bun add xterm@5",
+    "bun add xterm-addon-fit@0"
+];
     
     if (commands.length === 0) {
         console.log('⊘ No setup commands to run');
